de117a29c84ca027ce3767a33962fc165bc20e6d
dosss <dobi6658@gmail.com>
2021-11-13 17:59:30 +0800

    tailwind fi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       fontFamily: {
         Manrope: ['Manrope']
       }
     }
-  },
-  variants: {
-    extend: {}
   },
   plugins: []
 }
